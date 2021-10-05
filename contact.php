2a4e9c9193a00577e9aa44466d410631956a57ce
Virgin-ie <virginie.evrard.contact@gmail.com>
2021-10-05 18:45:22 +0200

    mise en place du menu et des fichiers du sites

---
+++ contact.php
@@ -0,0 +1,7 @@
+<?php
+require 'menu.php'; ?>
+
+<h1>Titre de la page</h1>
+
+<?php
+require 'footer.php'; ?>
---
+++ menu.php
@@ -0,0 +1,41 @@
+
+<!DOCTYPE html>
+    <html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!--Bootstrap CSS-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+        <!--Bootstrap Icon-->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+        <!--CSS-->
+        <link rel="stylesheet" type="text/css" href="style.css">
+
+        <title>F@stochePC11</title>
+    </head>
+
+    <body>
+        <div class="header">
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <div class="container-fluid">   
+                        <!-- F@stochePC11 renvoi sur la page d'accueil -->
+                        <a class="navbar-brand  order-0" id="fastocheHome"  href="index.php">F@stochePC11</a>   
+
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".dual-collapse2" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div class="collapse navbar-collapse w-100 justify-content-end dual-collapse2">
+                                <!-- Lien vers la page A propos -->
+                                <li class="nav-item active">
+                                    <a class="nav-link" aria-current="page" href="a_propos.php">A PROPOS </a>
+                                </li>
+                                <!-- Lien vers la page contact -->
+                                <li class="nav-item">
+                                    <a class="nav-link" href="contact.php">CONTACT</a>
+                                </li>
+                        </div>
+                </div>
+            </nav>
+        </div>
